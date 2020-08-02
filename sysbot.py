46d9a079af843b6f8aa7a4107060f06159f16e02
kisliy322 <vladvkontakt69@gmail.com>
2020-08-02 12:47:25 +0300

    test: change chat id

--- sysbot.py
+++ sysbot.py
@@ -1,11 +1,40 @@
 import telebot
+import requests
+import json
+import os
 
-bot = telebot.TeleBot('api')
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
-@bot.message_handler(commands=['start'])
-def start(message):
-	send_mess = f"<b>Привет {message.from_user.first_name} {message.from_user.last_name}</b>!"
-	bot.send_message(message.chat.id, send_mess, parse_mode='html')
+class Parser:
+
+	def __init__(self):
+		self.url = 'https://raw.githubusercontent.com/antonkurenkov/systembuilder/develop/status.json'
+
+	def parse(self):
+		return requests.get(self.url).json()
 
 
-bot.polling(none_stop=True)
+class Notifier:
+
+	def __init__(self, raw):
+		self.chat_id = 417554679 # anton
+		# self.chat_id = -420442510 # ksk2020
+		self.status = raw['status']
+		self.bot = telebot.TeleBot(TOKEN)
+
+	def prepare(self):
+		return f'''Статус сборки: {self.status[2]["status"]}
+		Версия релиза: {self.status[0]["release"]}
+		Дата релиза: {self.status[1]["datetime"][:10]}'''
+		
+
+	def send(self):
+		message = self.prepare()
+		self.bot.send_message(self.chat_id, message, parse_mode='html')
+
+
+if __name__ == "__main__":
+	parser = Parser()
+	status = parser.parse()
+	notifier = Notifier(status)
+	notifier.send()
